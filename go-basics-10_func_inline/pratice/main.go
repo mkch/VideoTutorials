8c7dc633dc32d4cdb1558874672ed3dec00517bf
Kevin Yuan <farproc@gmail.com>
2025-01-03 19:05:41 +0800

    go-basics-10 improved.

--- go-basics-10_func_inline/pratice/main.go
+++ go-basics-10_func_inline/pratice/main.go
@@ -5,31 +5,6 @@ import (
 	"slices"
 )
 
-// func main() {
-// 	AddListener(func() { fmt.Println("1") })
-// 	AddListener(func() { fmt.Println("2") })
-// 	CallListeners()
-// }
-
-// type Listener func()
-
-// var listeners []Listener
-
-// func CallListeners() {
-// 	for _, l := range listeners {
-// 		l()
-// 	}
-// }
-
-// func AddListener(l Listener) {
-// 	listeners = append(listeners, l)
-// }
-
-// func RemoveListener(l Listener) {
-// 	slices.DeleteFunc(listeners,
-// 		func(dl Listener) bool { return dl == l })
-// }
-
 func main() {
 	tag1 := AddListener(func() { fmt.Println("1") })
 	AddListener(func() { fmt.Println("2") })
@@ -41,29 +16,22 @@ func main() {
 
 type Listener func()
 
-type Tag struct{ *int }
+type Tag struct{ l *Listener }
 
-type taggedListener struct {
-	tag      Tag
-	listener Listener
-}
-
-var listeners []taggedListener
+var listeners []*Listener
 
 func CallListeners() {
 	for _, l := range listeners {
-		l.listener()
+		(*l)()
 	}
 }
 
 func AddListener(l Listener) Tag {
-	tag := Tag{new(int)}
-	listeners = append(listeners,
-		taggedListener{tag, l})
-	return tag
+	listeners = append(listeners, &l)
+	return Tag{&l}
 }
 
 func RemoveListener(tag Tag) {
 	listeners = slices.DeleteFunc(listeners,
-		func(d taggedListener) bool { return d.tag == tag })
+		func(d *Listener) bool { return d == tag.l })
 }
