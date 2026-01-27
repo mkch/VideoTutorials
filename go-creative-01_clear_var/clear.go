584a9c61444dc911efd9968de7b261b02336ef0b
Kevin Yuan <farproc@gmail.com>
2026-01-27 20:34:27 +0800

    Update package name in comments

--- go-creative-01_clear_var/clear.go
+++ go-creative-01_clear_var/clear.go
@@ -27,14 +27,14 @@ func Clear[T any](ptr *T) {
 //	go test -run ^BenchmarkClear -bench . -benchmem
 //	goos: darwin
 //	goarch: amd64
-//	pkg: github.com/mkch/gg
+//	pkg: helper
 //	cpu: Intel(R) Core(TM) i7-7920HQ CPU @ 3.10GHz
 //	BenchmarkClearSafeSmall-8       13269362                94.46 ns/op            0 B/op          0 allocs/op
 //	BenchmarkClearSmall-8           12717471                87.57 ns/op            0 B/op          0 allocs/op
 //	BenchmarkClearSafeLarge-8          18404             67189 ns/op          516105 B/op          1 allocs/op
 //	BenchmarkClearLarge-8             117871              9510 ns/op               0 B/op          0 allocs/op
 //	PASS
-//	ok      github.com/mkch/gg      5.062s
+//	ok      helper      5.062s
 func clearSafe[T any](ptr *T) {
 	var zero T
 	*ptr = zero
